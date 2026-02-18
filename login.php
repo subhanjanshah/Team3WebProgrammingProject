fea50acbd058ed1b4f67743e46e56f05047b4b3e
Maheesha Nuwangie Sumanadasa Akmeemana Madduma Acharige <maheesha53001@gmail.com>
2026-02-18 23:55:00 +0200

    Refactor login form and update action URLs

--- login.php
+++ login.php
@@ -7,27 +7,15 @@ include 'header.php';
     <div class="login-box">
         <h2>Welcome Back</h2>
         
-        <?php
-        if(isset($_SESSION['login_error'])) {
-            echo '<div class="error-message">' . $_SESSION['login_error'] . '</div>';
-            unset($_SESSION['login_error']);
-        }
-        
-        if(isset($_SESSION['login_success'])) {
-            echo '<div class="success-message">' . $_SESSION['login_success'] . '</div>';
-            unset($_SESSION['login_success']);
-        }
-        ?>
-        
-        <form action="authenticate.php" method="POST">
+        <form action="create.php" method="POST">
             <div class="form-group">
                 <label for="email">Email address</label>
                 <input type="email" id="email" name="email" placeholder="Enter your email" required>
             </div>
             
             <div class="form-group">
-                <label for="password">Password</label>
-                <input type="password" id="password" name="password" placeholder="Enter your password" required>
+                <label for="pswd">Password</label>
+                <input type="password" id="pswd" name="pswd" placeholder="Enter your password" required>
             </div>
             
             <div class="remember-me">
@@ -37,10 +25,6 @@ include 'header.php';
             
             <button type="submit" class="login-btn">Sign In</button>
         </form>
-    </div>
-    
-    <div class="copyright">
-        ©️ 2026 CGG Cafe. All rights reserved.
     </div>
 </div>
 
